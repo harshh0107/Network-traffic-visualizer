31eed57b15b76d3a5980a0eca28970e91d589e82
Harshini <126320221+harshh0107@users.noreply.github.com>
2024-12-19 10:37:19 +0530

    Add files via upload

---
+++ GOD_KNOWS.py
@@ -0,0 +1,43 @@
+import pyshark
+import os
+import pandas as pd
+import matplotlib as plt
+import matplotlib.pyplot as plt
+
+
+os.chdir("/Users/saiha/Documents/sharkii")
+# Open the capture file
+cap = pyshark.FileCapture('third.pcapng', keep_packets=True)  # Keep packets in memory for iteration
+
+# Try reading the first 5 packets
+try:
+    packet_count = 0
+    for packet in cap:
+        packet_count += 1
+        if packet_count <= 5:  # Print details for first 5 packets
+            print(packet)
+    print(f"Number of packets in capture: {packet_count}")
+finally:
+    cap.close()
+
+
+data = []
+for pkt in cap:
+    if 'IP' in pkt:
+        data.append({
+            'Time': pkt.sniff_time,
+            'Source': pkt.ip.src,
+            'Destination': pkt.ip.dst,
+            'Protocol': pkt.highest_layer,
+            'Length': pkt.length
+        })
+
+df = pd.DataFrame(data)
+
+protocol_data = df['Protocol'].value_counts()
+
+protocol_data.plot(kind='bar')
+plt.title('Traffic by Protocol')
+plt.xlabel('Protocol')
+plt.ylabel('Packet Count')
+plt.show()
---
+++ README.txt
@@ -0,0 +1,6 @@
+1. Download Wireshark on the host to capture files. Save it with pcap extension file.
+2. Save it at an accessible place.
+
+
+3. At line 8 give the path of directory where the file exists.
+4. At line 10 give the file name
